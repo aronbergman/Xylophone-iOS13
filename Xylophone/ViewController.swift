53107a6e8aab637f69c01199bc7bcf3f6799694d
aronbergman <brgmn@icloud.com>
2023-09-14 18:32:20 +0200

    feat(view): linking multiple buttons to the same IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,24 +1,24 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-    
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got pressed")
+        playSound()
+        
+        print(sender.currentTitle)
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
